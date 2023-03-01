e339c4fd3c88ba7232026d69e4cd137cf3db23d4
Ryan Hill <rch+git@zndx.org>
2023-03-01 15:33:59 -0700

    working

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Delopy";
+  description = "Provision machines in CloudCat";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
@@ -9,18 +9,12 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }: {
-    defaultPackage.x86_64-darwin = self.packages.x86_64-darwin.chk-setup;
+    defaultPackage.x86_64-darwin = self.packages.x86_64-darwin.cloudera-setup;
 
-    packages.x86_64-darwin.chk-setup = 
+    packages.x86_64-darwin.cloudera-setup = 
       let
-        system = "x86_64-darwin";
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            self.overlays.default
-          ];
-        };
-        var-name = "chk-setup";
+	pkgs = import nixpkgs { system = "x86_64-darwin"; };
+        var-name = "cloudera-setup";
         var-source = builtins.readFile ./deploy.sh;
         setup-machines = (pkgs.writeScriptBin var-name var-source).overrideAttrs(old: {
           buildCommand = "${old.buildCommand}\n patchShebangs $out";
@@ -35,16 +29,12 @@
           python310Packages.pyparsing
           python310Packages.sphinx
           python310Packages.tox
-          python310Packages.ansible-runner
         ]; 
-      in {
-        overlays.default =  (self: super: rec {});
-        pkgs.symlinkJoin = {
-          name = var-name;
-          paths = [ setup-machines ] ++ var-buildInputs;
-          buildInputs = [ pkgs.makeWrapper ];
-          postBuild = "wrapProgram $out/bin/${var-name} --prefix PATH : $out/bin";
-        };
+      in pkgs.symlinkJoin {
+        name = var-name;
+        paths = [ setup-machines ] ++ var-buildInputs;
+        buildInputs = [ pkgs.makeWrapper ];
+        postBuild = "wrapProgram $out/bin/${var-name} --prefix PATH : $out/bin";
       };
   };
 }
